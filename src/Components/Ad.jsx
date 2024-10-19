14c6bb594e69089eab8fc49a3ffc20dbdcbb62de
arnav <arnabdas2753@gmail.com>
2024-10-19 15:43:37 +0530

    done

--- src/Components/Ad.jsx
+++ src/Components/Ad.jsx
@@ -2,8 +2,11 @@ let img = "https://res.cloudinary.com/dpzpn3dkw/image/upload/v1729086658/downloa
 
 const Ad = () => {
   return (
-    <div className="flex justify-center items-center rounded-3xl bg-slate-400 h-[500px] w-[80%] mx-auto p-2 m-10 ">
-      <img src={img} className=" w-full m-10 h-full object-cover rounded-3xl" alt="Advertisement" /> 
+    <div className="text-4xl p-10 text-slate-800">
+      <div className="m-10">Download our Official App .....</div>
+      <div className="rounded-3xl bg-slate-400 h-[500px] w-[80%] mx-auto p-10"> 
+        <img src={img} className="w-full h-full object-cover rounded-3xl" alt="Advertisement" />
+      </div>
     </div>
   );
 }
